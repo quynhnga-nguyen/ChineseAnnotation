c8a598d41915aa30bc584e490270ad9dc8d6b08b
Nhan Nguyen <khanhnhan5391@gmail.com>
2021-06-06 12:34:36 -0700

    Changing single quotes to double quotes for consistency with the existing code

--- options.js
+++ options.js
@@ -1,12 +1,12 @@
 // Saves options to chrome.storage
 function saveOptions() {
-  var useLocalhost = document.getElementById('useLocalhost').checked;
+  var useLocalhost = document.getElementById("useLocalhost").checked;
   chrome.storage.sync.set({
     useLocalhost: useLocalhost,
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    var status = document.getElementById("status");
+    status.textContent = "Options saved.";
     setTimeout(function() {
       status.textContent = '';
     }, 750);
@@ -20,12 +20,12 @@ function restoreOptions() {
     useLocalhost: false,
   }, function(items) {
     console.log(items);
-    document.getElementById('useLocalhost').checked = items.useLocalhost;
+    document.getElementById("useLocalhost").checked = items.useLocalhost;
   });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
   restoreOptions();
-  document.getElementById('save').addEventListener('click',
+  document.getElementById("save").addEventListener("click",
       saveOptions);
 });
